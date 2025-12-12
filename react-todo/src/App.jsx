3d0f85ba9ec0ad645757b0e2d93a1d6c7821f3c6
Daniel Kofi Yamoah <dnnkofi@gmail.com>
2025-12-12 16:35:01 +0000

    feat: Integrate TestingDashboard component into App
    - Added TestingDashboard component to the main App for enhanced testing capabilities.
    - Updated App.jsx to include the new component alongside TodoList.

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,10 +1,12 @@
 import TodoList from "./components/TodoList";
+import TestingDashboard from "./components/TestingDashboard";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <TodoList />
+      <TestingDashboard />
     </div>
   );
 }
